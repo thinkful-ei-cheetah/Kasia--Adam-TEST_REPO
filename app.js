2d619199ea0f1a61d2c1aa8766215412462e1e6a
Kasia <kasia@miroartdesign.com>
2019-03-25 17:32:25 -0400

    functions

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
 const myTeam = 'Kasia & Adam';
 console.log(myTeam);
 
-console.log("Hi my name is Kasia and I'm 30 years old");
+function createGr(name, age){
+    const yearOfBirth = 2019 - age;
+ return `Hi my name is ${name} and I'm ${age} year old. I was born in ${yearOfBirth}`;
+}
+const greeting1 = createGr('jake', 35);
+console.log(greeting1);
+
+
+function getYearOfBirth(age){
+    const YOB = 2019 - age; 
+    if (age < 0){
+        throw new Error("Age can not be negative");
+    }
+    return YOB;
+}
+console.log(getYearOfBirth(-45));
+
+
